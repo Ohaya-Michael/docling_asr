d0c6f627071c3a56af0ac215d59fb0681cbe1077
Ohaya-Michael <michael.onyebuchi.ohaya@gmail.com>
2026-05-08 18:05:31 +0200

    Added render frontend to api cors

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ image_converter = build_image_converter()
 app = FastAPI(title="Docling Multi-Format API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://docling-asr-frontend.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
